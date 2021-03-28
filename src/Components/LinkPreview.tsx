c2e85c0a4fa796b411bd245d2ac772e43af73301
Tyler Menezes <tylermenezes@codeday.org>
2021-03-28 17:36:06 -0400

    Add link preview

---
+++ src/Components/LinkPreview.tsx
@@ -0,0 +1,60 @@
+import { ReactElement } from 'react';
+import { Text } from '@chakra-ui/react';
+import { ChakraProps } from '@chakra-ui/system';
+import { LinkIcon, EmailIcon } from '@chakra-ui/icons';
+
+interface LinkPreviewProps extends ChakraProps {
+  href?: string | undefined
+}
+
+function fixHref(link: string | undefined): string | undefined {
+  if (!link) return undefined;
+  const allowedPrefixes = ['http://', 'https://', 'mailto:'];
+  if (allowedPrefixes.reduce((accum: boolean, p) => accum || link.indexOf(p) === 0, false)) return link;
+  return `http://${link}`;
+}
+
+function cleanupHostname({ hostname }: URL): string {
+  const domainParts = hostname.split(/\./g);
+  if (domainParts[0] === 'www') return domainParts.slice(1).join('.');
+  return hostname;
+}
+
+function makeDisplayUrl(url: URL): string {
+  const cleanedHost = cleanupHostname(url);
+  const pathParts = url.pathname.split('/').filter(Boolean);
+
+  let displayPath: string | undefined;
+  if (pathParts[0]) {
+    if (cleanedHost === 'instagram.com') displayPath = pathParts[0];
+    else if ((cleanedHost === 'facebook.com' || cleanedHost === 'fb.com')) displayPath = pathParts[0];
+    else if (cleanedHost === 'linkedin.com' && pathParts[1]) displayPath = pathParts.slice(0,2).join('/');
+    else if (cleanedHost === 'youtube.com' && pathParts[0] === 'c') displayPath = pathParts.slice(0,2).join('/');
+    else if (cleanedHost === 'youtube.com' && pathParts[0] !== 'watch') displayPath = `${pathParts[0]}/…`;
+  }
+
+  return displayPath ? `${cleanedHost}/${displayPath}` : `${cleanedHost}${pathParts.length > 0 ? '/…' : ''}`;
+}
+
+export default function LinkPreview({ href, ...props }: LinkPreviewProps): ReactElement {
+  const fixedLink = fixHref(href);
+  if (!fixedLink) return <></>;
+
+  const url = new URL(fixedLink);
+
+  const display = url.protocol === 'mailto:'
+    ? <><EmailIcon /> {url.pathname}</>
+    : <><LinkIcon /> {makeDisplayUrl(url)}</>;
+
+  return <Text
+    as="a"
+    color="gray.400"
+    href={fixedLink}
+    target="_blank"
+    rel="noopener"
+    d="inline-block"
+    {...props}
+  >
+    {display}
+  </Text>;
+}
--- src/Components/Person.tsx
+++ src/Components/Person.tsx
@@ -1,7 +1,9 @@
 import { ReactElement } from 'react';
 import { ChakraProps } from '@chakra-ui/system';
 import { Box, Text, Image } from '@chakra-ui/react';
+import { LinkIcon } from '@chakra-ui/icons';
 import Credit from './Credit';
+import LinkPreview from './LinkPreview';
 import { airtable } from '../utils'
 
 interface PersonProps extends ChakraProps {
@@ -11,28 +13,17 @@ interface PersonProps extends ChakraProps {
 
 export function getDisplayName(person: airtable.Person): ReactElement {
   const discordLogo = <Image src="/discord.svg" d="inline-block" h="1.2em" />;
-  if (person.name && person.discord) return <>{person.name} ({discordLogo}{person.discord})</>;
+  if (person.name && person.discord) return <>{person.name}/{discordLogo}{person.discord}</>;
   if (person.discord) return <>{discordLogo}{person.discord}</>;
   return <>{person.name || ''}</>;
 }
 
-function fixHyperlink(link: string): string {
-  const allowedPrefixes = ['http://', 'https://', 'mailto:'];
-  if (allowedPrefixes.reduce((accum: boolean, p) => accum || link.substr(0, p.length) === p, false)) return link;
-  return `http://${link}`;
-}
 
 export default function Person({ person, withCredits, ...props }: PersonProps): ReactElement {
   return (
-    <Box
-      d="block"
-      {...(person.promotionLink
-          ? { as: 'a', href: fixHyperlink(person.promotionLink), target: '_blank', rel: 'noopener' }
-          : {})}
-      {...props}
-    >
-      <Text fontWeight="bold" fontSize="md" textDecoration={person.promotionLink ? 'underline' : undefined}>
-        {getDisplayName(person)}
+    <Box d="block" {...props}>
+      <Text fontWeight="bold" fontSize="md">
+        {getDisplayName(person)} <LinkPreview href={person.promotionLink} />
       </Text>
       <Box fontSize="sm" color="gray.500">
         {withCredits &&
