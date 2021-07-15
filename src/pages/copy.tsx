47afe9d29e8accbaa9221ad1a26c6d306b44d41b
Tyler Menezes <tylermenezes@codeday.org>
2021-07-15 14:06:59 -0400

    Add copyable credits page

--- src/pages/copy.tsx
+++ src/pages/copy.tsx
@@ -12,29 +12,25 @@ interface IndexProps extends CustomAppProps {
   people: airtable.Person[],
 }
 
+function maybePluralize(str: string): string {
+  if (str.slice(-4) === 'Team') return str;
+  if (str.slice(-1) === 's') return str;
+  return `${str}s`;
+}
+
 export default function Copy({ people }: IndexProps): ReactElement {
   return (
     <Page title="Copyable Credits">
-      <Text fontSize="4xl" fontWeight="bold" mb={8}>Copy By Team</Text>
       {peopleToTeams(people).map((team) => (
         <>
-          <Text fontWeight="bold">{team.name.toUpperCase()}<br /><br /></Text>
+          <Text fontWeight="bold" color="#000" fontSize="xl" textDecor="underline">
+            {maybePluralize(team.name)}
+          </Text>
           {team.people.map((person) => (
             <>
-              <Text>► {getDisplayName(person, true)}</Text>
-              {person.promotionLink && <Text>{person.promotionLink}</Text>}
-              <br />
+              <Text id={person.id} color="#000">{getDisplayName(person, true)}</Text>
             </>
           ))}
-          <br />
-        </>
-      ))}
-
-      <Text fontSize="4xl" fontWeight="bold" mb={8}>Copy Everyone</Text>
-      {people.map((person) => (
-        <>
-          <Text>► {getDisplayName(person)}</Text>
-          {person.promotionLink && <Text>{person.promotionLink}</Text>}
           <br />
         </>
       ))}
